2238a8ff26147dac3345eb0888652c666a365b86
Danny <d.ortixe@icloud.com>
2023-08-09 14:38:03 -0700

    Updated; debugged

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use csv;
+use std::error::Error;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>
+{
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records()
+    {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./nba_players.csv") {
+        eprintln!("{}", e);
+    }
 }
